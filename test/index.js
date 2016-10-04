1e44f1104667b2a7480c8215270549cc3d5c1a1a
flotob <florian@glatz-mahlberg.de>
2016-10-04 14:30:09 +0200

    working monkeypatch

---
+++ test/index.js
@@ -0,0 +1,19 @@
+'use strict'
+const expect = require('chai').expect
+const Web3 = require('../lib')
+
+describe('test upchain web3 client', () => {
+  it('should connect to a node', (done) => {
+    var web3 = new Web3('up', 'chain', 'testnet')
+    web3.eth.getBlock(0, (err, block) => {
+      if (err) {
+        return done(err)
+      }
+
+      expect(block).to.have.ownProperty('hash')
+      expect(block.hash).to.equal('0x0cd786a2425d16f152c658316c423e6ce1181e15c3295826d7c9904cba9ce303')
+
+      done()
+    })
+  })
+})
