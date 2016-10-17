d1e0f1cd2d684de8fbee5682b83be08c462e10ab
Levin Keller <post@levinkeller.de>
2016-10-17 20:18:43 +0200

    Building for standalone usage.

--- webpack.config.js
+++ webpack.config.js
@@ -4,13 +4,17 @@ module.exports = {
     './index.js'
   ],
   output: {
-    path: __dirname + '/build/',  // Path to where I want testBundle.js to be put
-    filename: 'bundle.js'
+    // export itself to a global var
+    libraryTarget: 'var',
+    // name of the global var: 'Foo'
+    library: 'UpchainWeb3',
+    filename: 'upchainweb3.js',
+    path: 'dist'
   },
   module: {
     loaders: [
       { test: /\.jsx$/, loaders: [ 'jsx-loader' ], exclude: /node_modules/ },
-      { test: /\.js?$/, loaders: [ 'babel' ], exclude: /node_modules/ },
+      { test: /\.js?$/, loaders: [ 'babel'], exclude: /node_modules/ },
       { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' },
       { test: /\.json?$/, loader: 'json-loader' }
     ]
