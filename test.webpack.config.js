9abd90290c0004a43b4638221cddf6078e31aa49
Levin Keller <post@levinkeller.de>
2016-10-17 11:52:00 +0200

    Adding webpack config for local tests.

---
+++ test.webpack.config.js
@@ -0,0 +1,26 @@
+const Webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+module.exports = {
+  entry: [
+    'mocha\!./lib/client.test.js' // The mocha loader command pointing to the test root. Note the ! is escaped.
+  ],
+  output: {
+    path: __dirname + '/client/test/',  // Path to where I want testBundle.js to be put
+    filename: 'testBundle.js'
+  },
+  plugins: [ new HtmlWebpackPlugin() ],
+  module: {
+    loaders: [
+      { test: /\.jsx$/, loaders: [ 'jsx-loader' ], exclude: /node_modules/ },
+      { test: /\.js?$/, loaders: [ 'babel' ], exclude: /node_modules/ },
+      { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' },
+      {
+        test: /\.json?$/,
+        loader: 'json-loader'
+      }
+    ]
+  },
+  externals: [ {
+    xmlhttprequest: '{XMLHttpRequest:XMLHttpRequest}'
+  } ]
+}
